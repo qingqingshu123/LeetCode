3a56e044b46694f6a34813705a55ba8d39b7a6fd
qingqingshu <qingqingshu1234@gmail.com>
2017-06-12 14:07:36 +0800

    添加CodeM 和String

--- src/Generate.java
+++ src/Generate.java
@@ -5,6 +5,14 @@ import java.util.List;
  * Created by jixiaoqiang on 2017/6/11.
  */
 public class Generate {
+    public static void main(String[] args) {
+        for (int i = 0; i <5 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                break;
+            }
+            System.out.println(11);
+        }
+    }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
         if(numRows < 1)
